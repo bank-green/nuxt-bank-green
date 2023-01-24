072bf195e93b3b5e98e49a781e308e39ff3dfed6
Simon <abeluga@posteo.net>
2023-01-24 14:28:48 +0100

    actually fix

--- components/LazyImage.vue
+++ components/LazyImage.vue
@@ -1,30 +1,32 @@
 <template>
-    <picture v-if="useImageTag">
-        <source type="image/webp" :srcset="srcsetOptionsWebP" />
-        <source :srcset="srcsetOptions" />
-        <img ref="img" :src="src" :alt="alt" :width="width" :height="height" loading="lazy"
-            class="transition-opacity ease-in-out duration-150" :class="[
-                imgClass,
-                {
-                    'opacity-0': !loaded,
-                    'opacity-100': loaded,
-                },
-            ]" @load="onLoad" />
-    </picture>
-    <!-- not in viewport -->
-    <span v-else-if="!hasBeenInViewport" ref="observer" class="block w-full h-full" />
-    <!-- scrolled to, loading -->
-    <img v-else-if="!loaded" ref="img" :src="src" :srcset="srcsetOptions" class="w-px h-px absolute opacity-0"
-        :alt="alt" @load="onLoad" />
-    <!-- transition can be disabled if we have loaded this image before and it's cached -->
-    <span v-else-if="!useTransition" :style="bgStyle" :currentSrc="currentSrc" class="block w-full h-full"
-        :class="imgClass" />
-    <!-- visible as a background image -->
-    <transition v-else enter-from-class="opacity-0" enter-active-class="transition ease-out duration-150"
-        enter-to-class="opacity-100" leave-from-class="opacity-100" leave-active-class="transition ease-in duration-200"
-        leave-to-class="opacity-0" appear>
-        <span :style="bgStyle" :currentSrc="currentSrc" class="block w-full h-full" :class="imgClass" />
-    </transition>
+    <ClientOnly>
+        <picture v-if="useImageTag">
+            <source type="image/webp" :srcset="srcsetOptionsWebP" />
+            <source :srcset="srcsetOptions" />
+            <img ref="img" :src="src" :alt="alt" :width="width" :height="height" loading="lazy"
+                class="transition-opacity ease-in-out duration-150" :class="[
+                    imgClass,
+                    {
+                        'opacity-0': !loaded,
+                        'opacity-100': loaded,
+                    },
+                ]" @load="onLoad" />
+        </picture>
+        <!-- not in viewport -->
+        <span v-else-if="!hasBeenInViewport" ref="observer" class="block w-full h-full" />
+        <!-- scrolled to, loading -->
+        <img v-else-if="!loaded" ref="img" :src="src" :srcset="srcsetOptions" class="w-px h-px absolute opacity-0"
+            :alt="alt" @load="onLoad" />
+        <!-- transition can be disabled if we have loaded this image before and it's cached -->
+        <span v-else-if="!useTransition" :style="bgStyle" :currentSrc="currentSrc" class="block w-full h-full"
+            :class="imgClass" />
+        <!-- visible as a background image -->
+        <transition v-else enter-from-class="opacity-0" enter-active-class="transition ease-out duration-150"
+            enter-to-class="opacity-100" leave-from-class="opacity-100"
+            leave-active-class="transition ease-in duration-200" leave-to-class="opacity-0" appear>
+            <span :style="bgStyle" :currentSrc="currentSrc" class="block w-full h-full" :class="imgClass" />
+        </transition>
+    </ClientOnly>
 </template>
 
 <script>
