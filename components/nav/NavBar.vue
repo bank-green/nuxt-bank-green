2eb833f22b845cc6cf89113c9b53bcec96eca328
Simon <abeluga@posteo.net>
2023-02-08 18:28:44 +0100

    change Take Action to Switch Survey

--- components/nav/NavBar.vue
+++ components/nav/NavBar.vue
@@ -34,11 +34,11 @@
                     <NuxtLink v-for="link in headerLinks" :key="link.href" :to="link.href"
                         activeClass="bg-primary-light" inactiveClass="hover:bg-primary-light"
                         class="px-4 py-2 text-base text-white font-medium rounded-xl" @click="isOpen = false">{{
-                                link.short_title || link.title
+                            link.short_title || link.title
                         }}</NuxtLink>
                     <NuxtLink to="/take-action" activeClass="bg-sushi-600"
                         class="inline w-auto px-4 py-2 font-medium bg-sushi-500 hover:bg-sushi-600 text-white text-center w-full rounded-xl shadow-green capitalize">
-                        Take action
+                        Switch Survey
                     </NuxtLink>
                 </div>
             </div>
@@ -55,7 +55,7 @@
                     @click="isOpen = false">{{ link.title }}</NuxtLink>
                 <NuxtLink to="/take-action"
                     class="block w-full text-left py-2 px-6 md:py-4 md:px-6 mt-0.5 md:mt-1 text-sm font-medium bg-primary-dark text-white rounded-xl bg-sushi-500 hover:bg-sushi-600">
-                    Take action
+                    Switch Survey
                 </NuxtLink>
             </div>
         </transition>
