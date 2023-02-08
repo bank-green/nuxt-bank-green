9c30808c781e480764a113ba9891d1f0a4327df4
Simon <abeluga@posteo.net>
2023-02-08 18:29:27 +0100

    add typeform like to eco banks

--- components/bank/detail/BankDetailGoodEthical.vue
+++ components/bank/detail/BankDetailGoodEthical.vue
@@ -19,16 +19,21 @@
                 <div class="max-w-xl mx-auto bg-primary-dark text-gray-100 rounded-xl shadow-xl p-8">
                     <div class="text-lg lg:text-xl font-medium mb-8 sm:px-16 lg:px-0">
                         {{ `Counting bank switches is vitally important to us as a movement. Please use this link when
-                                                signing up for ${details.name}.`
+                        signing up for ${details.name}.`
                         }}
                     </div>
-                    <a :href="details.website" target="_blank" class="button-green inline-block mx-auto">{{
-                            `Visit ${details.name}`
-                    }}</a>
+                    <NuxtLink tohref="details.website" target="_blank" class="button-green inline-block mx-auto">{{
+                    `Visit ${details.name}`
+                    }}</NuxtLink>
                     <NuxtLink to="/pledge"
                         class="block text-gray-200 hover:underline cursor-pointer font-bold mt-4 text-center">
                         Pledge to Switch Later
                     </NuxtLink>
+                    <div class="text-sm lg:text-md font-medium mt-8 sm:px-16 lg:px-0">
+                        Have you opened an account with {{ details.name }} since visiting Bank.Green? <NuxtLink
+                            class="text-sushi-300 hover:text-sushi-200"
+                            to="https://s3c0er2g1pf.typeform.com/to/rwxTfN15">Please let us know!</NuxtLink>
+                    </div>
                 </div>
             </div>
             <div v-if="details.rating === 'great'" class="flex items-end justify-end pointer-events-none">
