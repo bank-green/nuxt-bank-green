1a494b6ffad898408565ceb08407a209d9996368
Simon <32738398+shuesken@users.noreply.github.com>
2023-03-24 13:49:16 +0100

    automatically resize images to have width 400 on frontpage (#30)

--- slices/FeaturedInSlice/index.vue
+++ slices/FeaturedInSlice/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <img :src="slice.primary.logo.url" :alt="slice.primary.logo.alt" :class="slice.primary.class ?? 'h-10 w-auto'" />
+  <img :src='`${slice.primary.logo.url}&w=400`' :alt="slice.primary.logo.alt"
+    :class="slice.primary.class ?? 'h-10 w-auto'" />
 </template>
 
 <script>
