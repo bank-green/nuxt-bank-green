d6cf99e46dbb22bdd0967cd60bfa2e19fe627a01
Simon <abeluga@posteo.net>
2023-03-24 16:58:21 +0100

    dynamically load lottieplayer

--- composables/useLottieToggle.js
+++ composables/useLottieToggle.js
@@ -1,10 +1,11 @@
-import lottie from 'lottie-web/build/player/lottie_light'
 
-export default function useLottieToggle({ path, toggleState }) {
+export default async function useLottieToggle({ path, toggleState }) {
+
     const container = ref(null)
     const animData = ref(null)
 
-    onMounted(() => {
+    onMounted(async () => {
+        const lottie = await import('lottie-web/build/player/lottie_light')
         animData.value = lottie.loadAnimation({
             container: container.value, // the dom element that will contain the animation
             renderer: 'svg',
