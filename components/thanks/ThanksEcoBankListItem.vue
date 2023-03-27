e2ab8903160ae06bc8ee6a60ae4c97fda2548b08
Simon <abeluga@posteo.net>
2023-03-27 17:59:25 +0200

    rename widget only used on thanks page

---
+++ components/thanks/ThanksEcoBankListItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <NuxtLink :to="item ? `/sustainable-eco-banks/${item.tag}` : ''"
+        class="flex items-center justify-between rounded-2xl shadow-sm bg-white transition duration-150 ease-in-out hover:bg-gray-50 border border-transparent hover:border-sushi-500 py-3 pl-4 pr-5">
+        <transition enter-active-class="duration-200 transform-gpu origin-top ease-out"
+            enter-from-class="opacity-0 scale-y-95" enter-to-class="opacity-100 scale-y-100"
+            leave-active-class="duration-100 transform-gpu origin-top ease-in"
+            leave-from-class="opacity-100 scale-y-100" leave-to-class="opacity-0 scale-y-95" mode="out-in">
+            <div v-if="item" class="flex items-center truncate">
+                <div class="relative w-12 h-12">
+                    <ClearbitLogo v-if="item.website" :url="item.website" :size="24"
+                        imgClass="absolute inset-0 z-20 bg-contain bg-no-repeat bg-center bg-white" />
+                    <BankIcon class="absolute inset-0 z-10" />
+                </div>
+                <div class="ml-3 flex-1 font-semibold text-gray-800 block truncate">
+                    {{ item.name }}
+                </div>
+            </div>
+
+            <div v-else class="w-full flex items-center truncate">
+                <div class="w-12 h-12 bg-gray-100 rounded-xl animate-pulse"></div>
+                <div class="ml-3 flex-1 font-semibold text-gray-200 block truncate">
+                    loading...
+                </div>
+            </div>
+        </transition>
+
+        <svg class="text-sushi-500" width="7" height="13" viewBox="0 0 7 13" fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 11.5L6 6.5L1 1.5" stroke="currentColor" stroke-width="1.8" stroke-linecap="round"
+                stroke-linejoin="round" />
+        </svg>
+    </NuxtLink>
+</template>
+
+<script>
+import ClearbitLogo from '@/components/icons/ClearbitLogo'
+import BankIcon from '@/components/forms/banks/BankIcon'
+
+export default {
+    props: {
+        item: Object,
+    },
+    components: {
+        ClearbitLogo,
+        BankIcon,
+    },
+}
+</script>
---
+++ components/thanks/ThanksTopEcoBanksWidget.vue
@@ -0,0 +1,30 @@
+<template>
+    <LocationSearch class="mb-8 md:max-w-sm md:mx-auto z-50" v-model="country" />
+    <div v-if="banks.length" class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-min">
+        <ThanksEcoBankList :loaded="loaded" :list="banks" />
+    </div>
+    <div v-if="banks.length" class="sm:text-center">
+        <NuxtLink to="/sustainable-eco-banks" class="mt-6 button-green sm:inline-block sm:w-64">Show more</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+import LocationSearch from '@/components/forms/location/LocationSearch.vue'
+
+const { country } = useCountry()
+
+const banks = ref([])
+const loaded = ref(false)
+const loadBanks = async () => {
+    if (!country.value) {
+        return
+    }
+    banks.value = await getBanksList({
+        country: country.value,
+        topOnly: true,
+    })
+    loaded.value = true
+}
+onMounted(loadBanks)
+watch(country, loadBanks)
+</script>
