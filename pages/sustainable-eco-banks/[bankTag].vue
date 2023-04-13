9dfd96d9bd5b0e20888a45e7bf464b7249fc7162
Simon <abeluga@posteo.net>
2023-04-13 12:38:43 +0200

    add draft for pulling data from prismic

--- pages/sustainable-eco-banks/[bankTag].vue
+++ pages/sustainable-eco-banks/[bankTag].vue
@@ -1,5 +1,26 @@
 <template>
     <div class="page">
+        <div class="bg-gradient-to-b from-sushi-50 to-sushi-100 pt-28 md:mb-16">
+            <div class="page-fade-in contain max-w-5xl grid grid-cols-2 gap-8 md:gap-10">
+                <div class="prose">
+                    <h2>Founded</h2>
+                    <p>{{ sfi?.data.founded }}</p>
+
+                    <h2>Security</h2>
+                    <PrismicRichText :field="sfi?.data.security" />
+
+                    <h2>Impact</h2>
+                    <PrismicRichText :field="sfi?.data.impact" />
+
+                    <h2>Services</h2>
+                    <PrismicRichText :field="sfi?.data.services" />
+
+                    <h2>Convenience</h2>
+                    <PrismicRichText :field="sfi?.data.convenience" />
+                </div>
+
+            </div>
+        </div>
         <EcoBankHeader :name="details.name" :rating="details.rating" :subtitle="details.subtitle" :ourTake="details.ourTake"
             :website="details.website" :inheritBrandRating="details.inheritBrandRating" />
         <EcoBankDetail :details="details" :fromTheWebsite="details.fromTheWebsite" :name="details.name"
@@ -15,9 +36,14 @@
 // I think this issue is relevant: https://github.com/nuxt/framework/issues/8731
 const router = useRouter();
 const bankTag = router.currentRoute.value.params.bankTag;
-if (!bankTag)
+if (!bankTag || typeof bankTag !== 'string')
     throw new Error("no banktag supplied: " + JSON.stringify(router.currentRoute.value))
 const details = await getBankDetail(bankTag)
+
+const { client } = usePrismic();
+
+const { data: sfi } = await useAsyncData("sfi_" + bankTag, () =>
+    client.getByUID("sfipage", bankTag))
 
 useHeadHelper(`${details.name} Review and Service Offering - Bank.Green`)
 </script>
