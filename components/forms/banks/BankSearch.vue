68abc9e46b3547f0e9ff8cc19db44bdb4b7cceb9
Kien Dang <mail@kien.ai>
2023-04-25 11:40:26 +0800

    Fix some ts type errors flagged by Volar

--- components/forms/banks/BankSearch.vue
+++ components/forms/banks/BankSearch.vue
@@ -2,11 +2,11 @@
     <div v-if="hasBanks && loaded && !disabled" v-clickaway="hideList">
         <div class="relative">
             <SearchInput ref="input" :aria-expanded="isShowing" v-model="search" :placeholder="'Search bank...'"
-                @keydown.down="
-                    event => $refs['listPicker'].incrementFocus(event)
-                " @keydown.up="event => $refs['listPicker'].decrementFocus(event)" @keydown.enter="
-    event => $refs['listPicker'].selectCurrentItem()
-" @onFocus="showList" @onClick="showList" @onCloseClick="onCloseClick">
+                @keydown.down="(event: PointerEvent) => ($refs['listPicker'] as typeof ListPicker).incrementFocus(event)
+                    "
+                @keydown.up="(event: PointerEvent) => ($refs['listPicker'] as typeof ListPicker).decrementFocus(event)"
+                @keydown.enter="(_event: PointerEvent) => ($refs['listPicker'] as typeof ListPicker).selectCurrentItem()
+                    " @onFocus="showList" @onClick="showList" @onCloseClick="onCloseClick">
                 <template v-slot:icon>
                     <BankIcon class="h-6 w-6 absolute inset-0 m-4" />
                 </template>
@@ -15,9 +15,9 @@
             <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100"
                 leave-to-class="opacity-0">
                 <div v-if="isShowing" class="absolute z-10 mt-1 w-full rounded-md shadow-lg" :class="{
-                    'bg-white': filteredBanks.length > 0,
-                    'bg-gray-100': !filteredBanks.length,
-                }">
+                        'bg-white': filteredBanks.length > 0,
+                        'bg-gray-100': !filteredBanks.length,
+                    }">
                     <slot v-if="filteredBanks.length === 0" name="not-listed">
                         <NuxtLink to="/not-listed">
                             <div class="text-gray-500 text-center p-4 shadow-lg underline">
@@ -25,8 +25,7 @@
                             </div>
                         </NuxtLink>
                     </slot>
-                    <ListPicker ref="listPicker" v-else :items="filteredBanks" v-slot="{ item }"
-                        @selectItem="onSelectBank">
+                    <ListPicker ref="listPicker" v-else :items="filteredBanks" v-slot="{ item }" @selectItem="onSelectBank">
                         <BankSearchItem :id="item.tag" :name="item.name" :website="item.website"
                             :isSelected="item === modelValue" />
                     </ListPicker>
@@ -74,10 +73,10 @@ const loaded = ref(false);
 const search = ref('');
 const isShowing = ref(false);
 const selectedItem = ref(null);
-const input = ref(null);
+const input = ref<HTMLInputElement | null>(null);
 
 const hasBanks = computed(() => banks.value.length > 0);
-const filteredBanks = computed(() => 
+const filteredBanks = computed(() =>
     findBanks(banks.value, search.value)
 );
 
@@ -85,15 +84,15 @@ onMounted(() => {
     loadBanks();
 });
 
-watch(props.country, async function() {
+watch(props.country, async function () {
     await loadBanks();
     await nextTick();
-    if ((input.value && typeof(input.value.focus) == 'function') && (new Date() - pageStart > 15000)) {
+    if ((input.value) && (+new Date() - +pageStart > 15000)) {
         input.value.focus();
     }
 });
 
-watch(search, function(newValue) {
+watch(search, function (newValue) {
     if (props.modelValue && newValue != selectedItem.value) {
         emit('update:modelValue', null);
     }
@@ -110,7 +109,7 @@ async function loadBanks() {
 }
 
 function onKeyDown() {
-    if (search.value.length > 0 && loaded.value) 
+    if (search.value.length > 0 && loaded.value)
         showList();
 }
 
