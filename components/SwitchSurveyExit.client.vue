630f1634c88c48ddb89a98cf097485f53feea1fd
Kien Dang <mail@kien.ai>
2023-05-20 16:48:15 +0800

    Render prismic content
    Co-authored-by: Tho V <robert_vu947@protonmail.com>

--- components/SwitchSurveyExit.client.vue
+++ components/SwitchSurveyExit.client.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="flex flex-col items-center">
-        <p class="text-3xl mb-6 font-semibold whitespace-pre-wrap">
-            {{ title }}
-        </p>
-        <p class="text-lg mb-6 font-semibold whitespace-pre-wrap">
-            {{ subtitle }}
-        </p>
+        <PrismicRichText v-if="surveyContent?.data.title" :field="surveyContent.data.title" class="text-3xl mb-6 font-semibold whitespace-pre-wrap" />
+        <PrismicRichText v-if="surveyContent?.data.subtitle" :field="surveyContent.data.subtitle" class="text-lg mb-6 font-semibold whitespace-pre-wrap" /> 
         <NuxtLink @click="clickSurvey" to="/impact"
             class="px-6 py-3 text-medium no-underline bg-sushi-500 rounded hover:bg-sushi-600">
             Take a Short Survey
@@ -24,4 +20,7 @@ const emit = defineEmits(['success']);
 function clickSurvey() {
     emit('success');
 };
+
+const { client } = usePrismic()
+const { data: surveyContent } = await useAsyncData('switchsurveyexit', () => client.getSingle('switchsurveyexit'))
 </script>
