7216b737af878189546a3d42d342bc6ef6ff6eaf
Kien Dang <mail@kien.ai>
2023-05-22 23:55:39 +0800

    Migrate button text to Prismic

--- components/SwitchSurveyExit.client.vue
+++ components/SwitchSurveyExit.client.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="flex flex-col items-center">
-        <PrismicRichText v-if="surveyContent?.data.title" :field="surveyContent.data.title" class="text-3xl mb-6 font-semibold whitespace-pre-wrap" />
-        <PrismicRichText v-if="surveyContent?.data.subtitle" :field="surveyContent.data.subtitle" class="text-lg mb-6 font-semibold whitespace-pre-wrap" />
+        <PrismicRichText v-if="surveyContent?.data?.title" :field="surveyContent.data.title" class="text-3xl mb-6 font-semibold whitespace-pre-wrap" />
+        <PrismicRichText v-if="surveyContent?.data?.subtitle" :field="surveyContent.data.subtitle" class="text-lg mb-6 font-semibold whitespace-pre-wrap" />
         <NuxtLink @click="clickSurvey" to="/impact"
             class="px-6 py-3 text-medium no-underline bg-sushi-500 rounded hover:bg-sushi-600">
-            Take a Short Survey
+            <PrismicRichText v-if="surveyContent?.data?.buttontext" :field="surveyContent.data.buttontext" />
         </NuxtLink>
     </div>
 </template>
