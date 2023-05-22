4a1a73936dbb57548e8e941b69ccbd0dd02ffcf9
Tho Vu <robert_vu947@protonmail.com>
2023-05-22 12:16:33 +0800

    refactor FeaturedInSlice

--- slices/FeaturedInSlice/index.vue
+++ slices/FeaturedInSlice/index.vue
@@ -3,14 +3,10 @@
     :class="slice.primary.class ?? 'h-10 w-auto'" />
 </template>
 
-<script>
+<script setup lang="ts">
 import { getSliceComponentProps } from "@prismicio/vue";
 
-export default {
-  name: "FeaturedInSlice",
-  // The array passed to `getSliceComponentProps` is purely optional and acts as a visual hint for you
-  props: getSliceComponentProps(["slice", "index", "slices", "context"]),
-}
+defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]));
 </script>
 
 <style scoped></style>
