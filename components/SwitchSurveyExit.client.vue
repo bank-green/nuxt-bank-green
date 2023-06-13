731d5175cc236018f7f4c8d2fac9053447ea25e0
Simon <abeluga@posteo.net>
2023-06-13 14:58:34 +0300

    use link from prismic

--- components/SwitchSurveyExit.client.vue
+++ components/SwitchSurveyExit.client.vue
@@ -1,14 +1,16 @@
 <template>
-<Modal v-if="surveyContent != null" v-model="showModal">
-    <div class="flex flex-col items-center">
-        <PrismicRichText v-if="surveyContent?.data?.title" :field="surveyContent.data.title" class="text-3xl mb-6 font-semibold whitespace-pre-wrap" />
-        <PrismicRichText v-if="surveyContent?.data?.subtitle" :field="surveyContent.data.subtitle" class="text-lg mb-6 font-semibold whitespace-pre-wrap" />
-        <NuxtLink @click="clickSurvey" to="/impact"
-            class="px-6 py-3 text-medium no-underline bg-sushi-500 rounded hover:bg-sushi-600">
-            <PrismicRichText v-if="surveyContent?.data?.buttontext" :field="surveyContent.data.buttontext" />
-        </NuxtLink>
-    </div>
-</Modal>
+    <Modal v-if="surveyContent != null" v-model="showModal">
+        <div class="flex flex-col items-center">
+            <PrismicRichText v-if="surveyContent?.data?.title" :field="surveyContent.data.title"
+                class="text-3xl mb-6 font-semibold whitespace-pre-wrap" />
+            <PrismicRichText v-if="surveyContent?.data?.subtitle" :field="surveyContent.data.subtitle"
+                class="text-lg mb-6 font-semibold whitespace-pre-wrap" />
+            <NuxtLink @click="clickSurvey" :to="surveyContent?.data?.link?.url"
+                class="px-6 py-3 text-medium no-underline bg-sushi-500 rounded hover:bg-sushi-600">
+                <PrismicRichText v-if="surveyContent?.data?.buttontext" :field="surveyContent.data.buttontext" />
+            </NuxtLink>
+        </div>
+    </Modal>
 </template>
 
 <script setup lang="ts">
