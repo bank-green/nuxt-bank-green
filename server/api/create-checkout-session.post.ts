cf47ba4f5f4650d3beb27b88809dbee15c93fbcb
Simon <abeluga@posteo.net>
2023-07-10 13:59:32 +0200

    use http endpoints

--- server/api/create-checkout-session.post.ts
+++ server/api/create-checkout-session.post.ts
@@ -1,5 +1,6 @@
-import Stripe from 'stripe';
-import { CreateSubscriptionResponse } from '~~/utils/interfaces/donate';
+import Stripe from "stripe";
+import ofetch from "ofetch";
+import { CreateSubscriptionResponse } from "~~/utils/interfaces/donate";
 
 const domainURL = useRuntimeConfig().public.DOMAIN_URL as string;
 const stripeSecretKey = useRuntimeConfig().STRIPE_SECRET_KEY as string;
@@ -10,68 +11,73 @@ const priceId4 = useRuntimeConfig().STRIPE_SUBSCRIPTION_PRICE_4 as string;
 const priceId5 = useRuntimeConfig().STRIPE_SUBSCRIPTION_PRICE_5 as string;
 const priceId6 = useRuntimeConfig().STRIPE_SUBSCRIPTION_PRICE_6 as string;
 
-const stripe = new Stripe(stripeSecretKey, {
-    apiVersion: "2022-11-15",
-});
+const getPriceId = (amount: number): string | null => {
+  switch (amount) {
+    case 25:
+      return priceId1;
+    case 50:
+      return priceId2;
+    case 100:
+      return priceId3;
+    case 200:
+      return priceId4;
+    case 500:
+      return priceId5;
+    case 1000:
+      return priceId6;
+    default:
+      return null;
+  }
+};
 
-const getPriceId = (amount: number) : string | null =>  {
-    switch (amount) {
-        case 25:
-            return priceId1;
-        case 50:
-            return priceId2;
-        case 100:
-            return priceId3;
-        case 200:
-            return priceId4;
-        case 500:
-            return priceId5;
-        case 1000:
-            return priceId6;
-        default:
-            return null;
-    }
-}
-
-export default defineEventHandler(async (event) : Promise<CreateSubscriptionResponse | void> => {  
+export default defineEventHandler(
+  async (event): Promise<CreateSubscriptionResponse | void> => {
     try {
-        let body = await readBody(event);
-        if (body instanceof Uint8Array) {
-          body = JSON.parse(new TextDecoder().decode(body));
-        }
-        
-        const priceId = getPriceId(body.amount);
-        if (priceId == null)
-            throw new Error("Subscription not found");    
-        const session = await stripe.checkout.sessions.create({
-            mode: "subscription",
-            line_items: [
-                {
-                    price: priceId,
-                    quantity: 1,
-                },
-            ],
-            // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-            success_url: `${domainURL}/donate-complete?session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: `${domainURL}/donate-cancelled`,
-            // automatic_tax: { enabled: true }
-        });
+      let body = await readBody(event);
+      if (body instanceof Uint8Array) {
+        body = JSON.parse(new TextDecoder().decode(body));
+      }
 
-        return {
-            success: true,
-            redirectURL: session.url,
-            error: null,
-        }
+      const priceId = getPriceId(body.amount);
+      if (priceId == null) throw new Error("Subscription not found");
 
-        // return sendRedirect(event, session.url || '', 303);
+      // we have to build the request ourself because the Stripe SDK does not play well with Cloudflare
+      const reqBody = {
+        mode: "subscription",
+        "line_items[0][price]": priceId,
+        "line_items[0][quantity]": '1',
+        success_url: `${domainURL}/donate-complete?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${domainURL}/donate-cancelled`
+      };
+
+
+
+      const res = await $fetch(`https://api.stripe.com/v1/checkout/sessions`, {
+        method: "POST",
+        headers: {
+          authorization:
+            `Basic ${Buffer.from(stripeSecretKey + ':').toString('base64') }`,
+        },
+        body: new URLSearchParams(reqBody),
+        parseResponse: JSON.parse,
+      });
+
+      return {
+        success: true,
+        redirectURL: res.url,
+        error: null,
+      };
+
+      // return sendRedirect(event, session.url || '', 303);
     } catch (e) {
-        let _e:Error = e;
-        setResponseStatus(400);
-        return {
-            success: false,
-            redirectURL: null,
-            error: _e.message,
-        };
+      let _e: Error = e;
+      console.error(e);
+      setResponseStatus(400);
+      return {
+        success: false,
+        redirectURL: null,
+        error: _e.message,
+      };
     }
-    
-});
+  }
+);
--- server/api/create-payment-intent.post.ts
+++ server/api/create-payment-intent.post.ts
@@ -1,56 +1,63 @@
-import Stripe from 'stripe';
-import { CreatePaymentIntentResponse } from '~~/utils/interfaces/donate';
+import Stripe from "stripe";
+import { CreatePaymentIntentResponse } from "~~/utils/interfaces/donate";
 
 const stripeSecretKey = useRuntimeConfig().STRIPE_SECRET_KEY as string;
 
-const stripe = new Stripe(stripeSecretKey, {
-    apiVersion: "2022-11-15",
-});
 
 // https://stripe.com/docs/currencies#presentment-currencies
 const DEFAULT_CURRENCY = "USD";
 // https://stripe.com/docs/currencies#zero-decimal
 const DEFAULT_MULTIPLIER = 100;
 
-const isValidAmount = (amount : number) => [25, 50, 100, 200, 500, 1000].includes(amount);
+const isValidAmount = (amount: number) =>
+  [25, 50, 100, 200, 500, 1000].includes(amount);
 
-export default defineEventHandler(async (event) : Promise<CreatePaymentIntentResponse> => {  
+export default defineEventHandler(
+  async (event): Promise<CreatePaymentIntentResponse> => {
     try {
-        let body = await readBody(event);
-        if (body instanceof Uint8Array) {
-          body = JSON.parse(new TextDecoder().decode(body));
-        }
+      let body = await readBody(event);
+      if (body instanceof Uint8Array) {
+        body = JSON.parse(new TextDecoder().decode(body));
+      }
 
-        if (!isValidAmount(body.amount)) {
-            return {
-                success: false,
-                clientSecret: null,
-                error: 'Invalid amount',
-            };
-        }
-
-        // Create a PaymentIntent with the order amount and currency
-        const paymentIntent = await stripe.paymentIntents.create({
-            amount: body.amount * DEFAULT_MULTIPLIER,
-            currency: DEFAULT_CURRENCY,
-            automatic_payment_methods: {
-                enabled: true,
-            },
-        });
-
+      if (!isValidAmount(body.amount)) {
         return {
-            success: true,
-            clientSecret: paymentIntent.client_secret as string,
-            error: null,
+          success: false,
+          clientSecret: null,
+          error: "Invalid amount",
         };
+      }
+
+      // we have to build the request ourself because the Stripe SDK does not play well with Cloudflare
+      const reqBody = {
+        amount: `${body.amount * DEFAULT_MULTIPLIER}`,
+        currency: DEFAULT_CURRENCY,
+        'automatic_payment_methods[enabled]': 'true'
+      };
+
+      const res = await $fetch(`https://api.stripe.com/v1/payment_intents`, {
+        method: "POST",
+        headers: {
+            authorization:
+              `Basic ${Buffer.from(stripeSecretKey + ':').toString('base64') }`,
+          },
+        body: new URLSearchParams(reqBody),
+        parseResponse: JSON.parse,
+      });
+
+      return {
+        success: true,
+        clientSecret: res.client_secret,
+        error: null,
+      };
     } catch (e) {
-        let _e:Error = e;
-        setResponseStatus(400);
-        return {
-            success: false,
-            clientSecret: null,
-            error: _e.message,
-        }
+      let _e: Error = e;
+      setResponseStatus(400);
+      return {
+        success: false,
+        clientSecret: null,
+        error: _e.message,
+      };
     }
-    
-  })
+  }
+);
