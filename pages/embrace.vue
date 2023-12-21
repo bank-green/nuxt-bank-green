5e6c06887bc91568540f7870e07cd5eee3a70cd2
sopoour <sophia.auer@googlemail.com>
2023-12-21 17:14:02 +0100

    adjust padding

--- pages/embrace.vue
+++ pages/embrace.vue
@@ -3,7 +3,7 @@
     <div class="page-fade-in overflow-hidden max-w-screen">
       <div class="bg-gradient-to-b from-sushi-50 to-sushi-100 pt-28">
         <div
-          class="contain flex flex-col justify-center items-center pb-4 lg:pb-0 mb-4"
+          class="contain flex flex-col justify-center items-center md:pb-20 pb-10 mb-4"
         >
           <h1
             class="max-w-3xl text-2xl font-semibold whitespace-pre-line mb-2 text-center"
