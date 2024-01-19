d70064c2d13ed5fd79603cb65d3245b61c10753b
ahmedlamidi <111273639+ahmedlamidi@users.noreply.github.com>
2024-01-19 04:39:01 +0000

    Fix gpe page

--- pages/GPE.vue
+++ pages/GPE.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="page">
+    <div bg="bg-sushi-50">
+      <SliceZone
+        :components="sliceComps"
+        :slices="gpe?.data.slices ?? []"
+        />
+    </div>
     <div class="bg-white">
       <div class=" page-fade-in grid grid-cols-2 gap-4 pt-28">
-        <SliceZone
-          :components="sliceComps"
-          :slices="gpe?.data.slices ?? []"
-        />
         <TextWithLogo
           :title="gpe?.data.leafslice1.at(0)?.title!"
           :img="asImageSrc(gpe?.data.leafslice1.at(0)?.image)!"
@@ -13,20 +15,14 @@
         />
       </div>
     </div>
-    <div class="bg-sushi-50">
-      <h1>
-        Hey
-      </h1>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { asImageSrc } from '@prismicio/helpers'
 import { defineSliceZoneComponents } from '@prismicio/vue'
 import { components } from '~~/slices'
 const sliceComps = ref(defineSliceZoneComponents(components))
-
-import { asImageSrc } from '@prismicio/helpers'
 const { client } = usePrismic()
 const { data: gpe } = await useAsyncData('gpe', () =>
   client.getSingle('greenpolicyevaluatorpage'))
