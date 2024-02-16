2533152bc04864d62b6badf24b6628cbdf8b28b3
Albert Carter <albert.r.carter@gmail.com>
2024-02-16 15:40:07 +0100

    Revert "Deleted The Button (#109)"
    This reverts commit a5748ee1a9d67e06573a7a1ae9a1bfe67fcd110a.

--- components/bank/BankLayoutGreatGoodOkUnknown.vue
+++ components/bank/BankLayoutGreatGoodOkUnknown.vue
@@ -70,6 +70,13 @@
           >
             Did we get something wrong?
           </h2>
+          <NuxtLink
+            :to="`https://data.bank.green/update/${bankTag}`"
+            target="_blank"
+            class="button-green inline-block w-max"
+          >
+            Help us fix it
+          </NuxtLink>
         </div>
 
         <div class="w-full flex flex-col items-center p-6 md:p-8">
@@ -101,4 +108,6 @@ const checkList = [
   'Discover other ways to divest from fossil fuels'
 ]
 
+const router = useRouter()
+const bankTag = router.currentRoute.value.params.bankTag
 </script>
