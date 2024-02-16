cbdd785461251c37c524aedae97c6b0275be90bc
Adnan Haramy <145838958+AdnanHarami@users.noreply.github.com>
2024-02-16 20:11:29 +0300

    Deleted HelpUs Section from bank layout pages (#115)

--- components/bank/BankLayoutGreatGoodOkUnknown.vue
+++ components/bank/BankLayoutGreatGoodOkUnknown.vue
@@ -68,21 +68,6 @@
           <h2
             class="w-full text-center font-semibold text-2xl tracking-wider mb-4 text-gray-100"
           >
-            Did we get something wrong?
-          </h2>
-          <NuxtLink
-            :to="`https://data.bank.green/update/${bankTag}`"
-            target="_blank"
-            class="button-green inline-block w-max"
-          >
-            Help us fix it
-          </NuxtLink>
-        </div>
-
-        <div class="w-full flex flex-col items-center p-6 md:p-8">
-          <h2
-            class="w-full text-center font-semibold text-2xl tracking-wider mb-4 text-gray-100"
-          >
             How do we derive our results?
           </h2>
           <NuxtLink
@@ -108,6 +93,4 @@ const checkList = [
   'Discover other ways to divest from fossil fuels'
 ]
 
-const router = useRouter()
-const bankTag = router.currentRoute.value.params.bankTag
 </script>
