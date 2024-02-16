ec56868b0e6975b13ff783ae64c729942e0e8bf0
Adnan Haramy <adnanharamy@hotmail.com>
2024-02-16 18:46:16 +0300

    Deleted HelpUs Section from bank layout pages

--- components/bank/BankLayoutBadWorst.vue
+++ components/bank/BankLayoutBadWorst.vue
@@ -143,21 +143,6 @@
           <h2
             class="w-full text-center font-semibold text-2xl tracking-wider mb-4 text-gray-100"
           >
-            Did we get something wrong?
-          </h2>
-          <NuxtLink
-            :to="`https://data.bank.green/update/${bankTag}`"
-            target="_blank"
-            class="button-green inline-block w-max"
-          >
-            Help us fix it
-          </NuxtLink>
-        </div>
-
-        <div class="w-full flex flex-col items-center p-6 md:p-8">
-          <h2
-            class="w-full text-center font-semibold text-2xl tracking-wider mb-4 text-gray-100"
-          >
             How do we derive our results?
           </h2>
           <NuxtLink
@@ -181,6 +166,4 @@ import Swoosh from '@/components/Swoosh.vue'
 import CallToAction from '@/components/CallToAction.vue'
 import ArrowDownBounce from '@/components/icons/ArrowDownBounce.vue'
 
-const router = useRouter()
-const bankTag = router.currentRoute.value.params.bankTag
 </script>
