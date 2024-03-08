9b2c508c4af7d6aa1ed4ceaf970b9ec7eda1fac8
Albert Carter <albert.r.carter@gmail.com>
2024-03-08 18:35:42 +0100

    feat: move everest bank rating methodology page (#121)

--- pages/everest/index.vue
+++
@@ -1,7 +0,0 @@
-<script>
-export default {
-  mounted () {
-    window.location.replace('https://moneymovement.sharepoint.com/:w:/s/allcompany/ERzKcLvwFPZGrV_OxkbWZBoBvbMubiGOtCahJMR8YBN0NA?e=Nu0jFk')
-  }
-}
-</script>
