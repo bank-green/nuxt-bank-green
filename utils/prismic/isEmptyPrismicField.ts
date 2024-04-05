ee258fb69277ba6214d7b50c7648b0d443a89ffb
Tho V <robert_vu947@protonmail.com>
2024-04-06 00:45:19 +0800

    fix GROOT-155: description 2 text not showing (#125)
    * add util to check empty prismic field
    
    * correct prismic checking logic
    
    * feat: corrrect description2 empty check on bad bank page
    
    * fix: typo
    
    * fix: typo
    
    ---------
    
    Co-authored-by: Albert Carter <albert.r.carter@gmail.com>

---
+++ utils/prismic/isEmptyPrismicField.ts
@@ -0,0 +1,27 @@
+/**
+ * An util function to check for empty Prismic field, regardless of types
+ */
+import { RTEmbedNode, RTImageNode, RTNode, RTTextNode, RichTextNodeType } from '@prismicio/types'
+
+function isRTNode (nodeObj: any): nodeObj is RTNode {
+  return 'type' in nodeObj
+}
+function isEmptyRTNode (nodeObj: RTNode): boolean {
+  return (nodeObj.type === RichTextNodeType.image && (nodeObj as RTImageNode).url.length > 0) ||
+    (nodeObj.type === RichTextNodeType.embed && !!(nodeObj as RTEmbedNode).oembed) ||
+    (nodeObj as RTTextNode).text.length > 0
+}
+
+export default function (fieldData: any): boolean {
+  const isTextField = typeof fieldData === 'string'
+  if (isTextField) {
+    return fieldData.length === 0
+  }
+  const isRichTextField = Array.isArray(fieldData) && fieldData.every(isRTNode)
+  if (isRichTextField) {
+    return (
+      fieldData.length === 0 || fieldData.every(node => !isEmptyRTNode(node))
+    )
+  }
+  return true
+}
