eba9816d00909497fe7749b1438086472941e4d7
Soph <36188360+sopoour@users.noreply.github.com>
2024-04-05 17:45:31 +0200

    Methodology Page (#124)
    * Created page
    
    * add generic underline class
    
    * remove factors description unused
    
    * change font sizes

--- components/TextWithImage.vue
+++ components/TextWithImage.vue
@@ -5,7 +5,7 @@
       :class="imgClasses || 'w-full'"
     />
     <div class="flex flex-col prose lg:prose-lg">
-      <h3 class="md:!text-xl lg:!text-2xl !font-bold">
+      <h3 v-if="title" class="md:!text-xl lg:!text-2xl !font-bold">
         {{ title }}
       </h3>
       <p class="!text-ocean-800 !text-opacity-75">
@@ -17,7 +17,7 @@
 
 <script setup lang="ts">
 defineProps<{
-  title: string,
+  title?: string,
   img: string,
   description: string
   imgClasses?: string
