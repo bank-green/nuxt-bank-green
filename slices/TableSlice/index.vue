2aef97278cd742594a6dc8f2c266ba6705f836a2
Tho Vu <robert_vu947@protonmail.com>
2024-05-30 00:59:42 +0800

    add new table_slice

---
+++ slices/TableSlice/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section
+    :data-slice-type="slice.slice_type"
+    :data-slice-variation="slice.variation"
+  >
+    Placeholder component for table_slice (variation: {{ slice.variation }})
+    Slices
+  </section>
+</template>
+<script setup lang="ts">
+import { getSliceComponentProps } from '@prismicio/vue'
+defineProps(getSliceComponentProps(['slice', 'index', 'slices', 'context']))
+</script>
