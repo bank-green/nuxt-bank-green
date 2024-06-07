5adfaa091c36553066ee174da9aa5e4821520f46
sopoour <sophia.auer@googlemail.com>
2024-06-07 14:41:50 +0200

    Fix missing img error

--- components/team/TeamMember.vue
+++ components/team/TeamMember.vue
@@ -1,13 +1,16 @@
 <template>
   <li class="flex flex-col gap-4 items-center w-full md:max-w-xs lg:max-w-sm justify-self-center">
-    <NuxtImg
-      :src="img"
-      width="128"
-      height="128"
-      provider="none"
-      class="object-center object-cover w-32 h-32 shadow-lg rounded-full overflow-hidden bg-gray-900"
-      loading="lazy"
-    />
+    <span class="w-32 h-32 bg-gray-200 rounded-full">
+      <NuxtImg
+        v-if="!!img"
+        :src="img"
+        width="128"
+        height="128"
+        provider="none"
+        class="object-center object-cover w-32 h-32 shadow-lg rounded-full overflow-hidden bg-gray-900"
+        loading="lazy"
+      />
+    </span>
     <a v-if="href" ref="noopener" :href="href" target="_blank" class="text-xl leading-6 font-semibold hover:underline">{{
       name
     }}</a>
