79db635abc67c0824c7fccd926ceecca790dcb85
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-08-28 14:04:44 +0200

    Add stylistic rule to keep single quotes and avoid escaping

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs';
+import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   {
+    ignores: ['prismicio-types.d.ts', './slices/index.ts'],
     rules: {
       'no-console': 'off',
       'vue/multi-word-component-names': 'off',
@@ -12,6 +13,7 @@ export default withNuxt(
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
       '@stylistic/max-statements-per-line': 'warn',
+      '@stylistic/quotes': ['warn', 'single', { avoidEscape: true }],
     },
   },
-);
+)
