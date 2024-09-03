0e7b3020ac63399ebadec7ae38d4edfd2b388f87
Soph <36188360+sopoour@users.noreply.github.com>
2024-09-03 20:10:45 +0200

    feature / create accredited page in prismic repeatable (#167)
    * created prismic custom type with fields
    
    * created broad setup for the new page in nuxt
    
    * add description to project
    
    * change the way how we get slug

---
+++ pages/accreditation/[slug].vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page">
+    <section class="bg-gradient-to-b from-sushi-50 to-sushi-100 md:pt-28 max-w-screen ">
+      <div class="page-fade-in contain flex flex-col justify-center items-center max-w-8xl pt-28 pb-20 md:pb-0 lg:pt-12 gap-10">
+        <h1>{{ accred?.data.hero_title || "Your money is building a greener future" }}</h1>
+      </div>
+      <Swoosh />
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts">
+/* import { asImageSrc } from '@prismicio/helpers'
+import { defineSliceZoneComponents } from '@prismicio/vue'
+import { components } from '~~/slices' */
+
+const route = useRoute()
+/* const error = ref(false) */
+
+const { client } = usePrismic()
+const slug = route.params.slug ?? ''
+const { data: accred } = await useAsyncData(slug[0], () =>
+  client.getByUID('accreditationpage', slug[0])
+)
+
+/*
+usePrismicSEO(gpe.value?.data) */
+
+</script>
