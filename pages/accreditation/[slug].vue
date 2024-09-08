1e31f7aa62dc8adee78203110667c0bad603025e
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-09-08 15:09:21 +0200

    Add certification and projects

--- pages/accreditation/[slug].vue
+++ pages/accreditation/[slug].vue
@@ -1,18 +1,41 @@
 <template>
   <div class="page">
-    <section class="bg-gradient-to-b from-sushi-50 to-sushi-100 md:pt-28 max-w-screen">
-      <div class="page-fade-in relative contain max-w-8xl pt-28 pb-20 md:pb-0 lg:pt-12 z-10">
-        <article class="rounded-2xl w-full max-w-7xl px-1 py-8 bg-white flex flex-col items-center text-center gap-y-10">
-          <h1 class="text-3xl lg:text-5xl font-semibold text-gray-800 green-line">
-            {{ accred?.data.hero_title || "Your money is building a greener future" }}
+    <section class="bg-gradient-to-b from-sushi-50 to-sushi-100 pt-28 max-w-screen">
+      <div class="page-fade-in relative contain pb-20 lg:pt-12 z-10">
+        <article class="relative flex flex-col items-center border rounded-2xl w-full max-w-7xl min-h-[504px] px-4 py-10 bg-white text-center gap-10">
+          <h1 class="text-3xl lg:text-5xl font-semibold text-gray-800">
+            <template v-if="accred?.data.hero_title">
+              {{ accred?.data.hero_title }}
+            </template>
+            <template v-else>
+              Your money is building a <span class="green-line">greener future</span>
+            </template>
           </h1>
-          <p class="text-gray-700 max-w-4xl">
-            By choosing to spend your money with businesses like [business name], you are ensuring your financial
-            support contributes to a sustainable, liveable future.
-          </p>
-          <p class="text-gray-700 max-w-4xl">
-            <span class="font-bold">[Business name]</span> banks with <span class="font-bold">[green bank name]</span> which means your money is supporting projects like:
-          </p>
+          <div class="lg:px-16 text-base lg:text-xl text-gray-700 max-w-4xl">
+            <template v-if="accred?.data.hero_description">
+              {{ accred.data.hero_description }}
+            </template>
+            <template v-else>
+              <p class="pb-6">
+                By choosing to spend your money with businesses like <b class="font-bold">[business name]</b>, you are
+                ensuring your financial
+                support contributes to a sustainable, liveable future.
+              </p>
+              <p>
+                <b class="font-bold">[Business name]</b> banks with <span class="font-bold">[green bank name]</span>
+                which means your money is supporting projects like:
+              </p>
+            </template>
+          </div>
+          <AccreditationProjectCarousel
+            class="lg:absolute lg:bottom-0 lg:left-80 lg:translate-y-16"
+            :projects="accred?.data.projects"
+          />
+          <img
+            class="absolute bottom-0 lg:left-36 w-32 h-32 translate-y-16 lg:translate-y-10"
+            src="/img/certification/fossil-free-certified.png"
+            alt="Fossil Free Certification"
+          >
         </article>
       </div>
       <Swoosh />
@@ -21,12 +44,7 @@
 </template>
 
 <script setup lang="ts">
-/* import { asImageSrc } from '@prismicio/helpers'
-import { defineSliceZoneComponents } from '@prismicio/vue'
-import { components } from '~~/slices' */
-
 const route = useRoute()
-/* const error = ref(false) */
 
 const { client } = usePrismic()
 const slug = route.params.slug ?? ''
@@ -44,6 +62,8 @@ usePrismicSEO(gpe.value?.data) */
   background-image: url('/img/backgrounds/green-line.svg');
   background-position: 100% 100%;
   background-repeat: no-repeat;
-  background-size: 35%;
+  background-size: 95%;
+  word-break: keep-all;
+  white-space: nowrap;
 }
 </style>
