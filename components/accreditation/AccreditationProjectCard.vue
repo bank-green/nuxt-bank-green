384971f8a41aaa674e850526467d0aacd91c184e
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-09-08 15:07:54 +0200

    Add accreditation project card for carousel

---
+++ components/accreditation/AccreditationProjectCard.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { asImageSrc } from '@prismicio/helpers'
+import PinIcon from '~/components/forms/location/PinIcon.vue'
+import { AccreditationpageDocumentDataProjectsItem } from 'prismicio-types'
+
+defineProps<Pick<AccreditationpageDocumentDataProjectsItem, 'title' | 'location' | 'image'>>()
+</script>
+
+<template>
+  <article class="relative flex flex-col justify-end items-start p-6 gap-2 rounded-xl w-[347.12px] h-[240px] overflow-hidden">
+    <h3 class="z-10 flex items-center gap-1 rounded-full w-fit pl-2 pr-3 py-1.5 bg-[#F6E9BA] ">
+      <PinIcon class="w-4 h-4" />
+      <span class="text-sm text-gray-700">
+        {{ location }}
+      </span>
+    </h3>
+    <h2 class="text-2xl font-bold text-white z-10">
+      {{ title }}
+    </h2>
+    <NuxtImg
+      v-if="image.url"
+      alt=""
+      class="absolute inset-0 z-0 w-full h-full object-cover rounded-xl"
+      loading="lazy"
+      :src="title?.includes('Project Default') ? image.url : asImageSrc(image)"
+      :provider="title?.includes('Project Default') ? 'none' : ''"
+    />
+  </article>
+</template>
