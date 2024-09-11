6ed2deb233bc1c2b1acd6359650bcd36998850dd
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-09-11 10:47:24 +0200

    Refactor carousel - Change order: template first, then script - Use unordered list - Filter out undefined/null project_links to fix typescript error

--- components/accreditation/AccreditationProjectCarousel.vue
+++ components/accreditation/AccreditationProjectCarousel.vue
@@ -1,32 +1,28 @@
+<template>
+  <ul class="grid lg:flex place-content-center gap-8 pb-32 lg:pb-0">
+    <li v-for="project in filterProjects" :key="project.project_link">
+      <a v-if="project.project_link" style="grid-area: 1/1" :href="project.project_link">
+        <AccreditationProjectCard :title="project.title" :location="project.location" :image="project.image" />
+      </a>
+    </li>
+  </ul>
+</template>
+
 <script lang="ts" setup>
 // TODO: add functionality
 import { defaultProject } from './default-project'
 import { AccreditationpageDocumentDataProjectsItem } from 'prismicio-types'
 
+type Project = Pick<AccreditationpageDocumentDataProjectsItem, 'title' | 'location' | 'image' | 'project_link'>;
+type ProjectWithLink = Project & { project_link: string };
+
 interface Props {
-    projects: Pick<AccreditationpageDocumentDataProjectsItem, 'title' | 'location' | 'image' | 'project_link'>[]
+  projects: Project[]
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   projects: () => ([defaultProject, { ...defaultProject, title: 'Project Default B' }])
 })
-</script>
 
-<template>
-  <div class="grid lg:flex place-content-center gap-8 pb-32 lg:pb-0">
-    <template v-for="project in projects">
-      <a
-        v-if="project.project_link"
-        :key="project.project_link"
-        style="grid-area: 1/1"
-        :href="project.project_link"
-      >
-        <AccreditationProjectCard
-          :title="project.title"
-          :location="project.location"
-          :image="project.image"
-        />
-      </a>
-    </template>
-  </div>
-</template>
+const filterProjects = computed(() => props.projects.filter(p => typeof p.project_link === 'string') as ProjectWithLink[])
+</script>
