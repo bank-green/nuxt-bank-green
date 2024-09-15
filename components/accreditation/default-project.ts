9403b23ed2965bd8f4acddbaf52313734d9d2b77
Claudia Benito <44007726+claudiabdm@users.noreply.github.com>
2024-09-15 09:54:12 +0200

    feat: hero section for accreditation page (#170)
    * Add title and paragraphs styles
    
    * Add accreditation project card for carousel
    
    * Add accreditation project carousel without functionality
    
    * Add certification and projects
    
    * Refactor carousel
    - Change order: template first, then script
    - Use unordered list
    - Filter out undefined/null project_links to fix typescript error
    
    * Change logo alt
    
    * Refactor
    - Move anchor from carousel to card
    - Add link prop
    - Use p
    - Change order template first, then script
    
    * Refactor [slug].vue
    - Create AccreditationHero
    - Replace article with div
    - Remove placeholders
    - Use logo from prismic when available

---
+++ components/accreditation/default-project.ts
@@ -0,0 +1,16 @@
+import img from './default-project-image.jpg'
+
+export const defaultProject = {
+  title: 'Project Default',
+  location: 'City, Country',
+  image: {
+    alt: 'Project Image Example',
+    copyright: '',
+    url: img,
+    dimensions: {
+      width: 347.12,
+      height: 100
+    }
+  },
+  project_link: 'link-to-project'
+}
