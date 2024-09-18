e07e253247d2f31e01c1fde3f79a2f089cdd7999
sopoour <sophia.auer@googlemail.com>
2024-09-18 16:41:33 +0200

    add contacts to Unconfirmed list as default

--- server/api/lead-gen-active-campaign.post.ts
+++ server/api/lead-gen-active-campaign.post.ts
@@ -1,15 +1,15 @@
 type FormFields = {
-  firstName: string,
-  email: string,
-  bankName: string,
-  status: string,
+  firstName: string
+  email: string
+  bankName: string
+  status: string
   marketing: string
 }
 
 export default defineEventHandler(
   async (event) => {
     try {
-      let body: {formFields: FormFields, bankLeadList: boolean} = await readBody(event)
+      let body: { formFields: FormFields, bankLeadList: boolean } = await readBody(event)
       if (body instanceof Uint8Array) {
         body = JSON.parse(new TextDecoder().decode(body))
       }
@@ -23,68 +23,61 @@ export default defineEventHandler(
           fieldValues: [
             {
               field: '2',
-              value: body.formFields.bankName
+              value: body.formFields.bankName,
             },
             {
               field: '18',
-              value: body.formFields.status
+              value: body.formFields.status,
             },
             {
               field: '11',
-              value: body.formFields.marketing
-            }
-          ]
-        }
+              value: body.formFields.marketing,
+            },
+          ],
+        },
       }
 
       // Create contact with the incoming fields
       const sendActiveCampaignForm: any = await $fetch(baseUrl + '/contact/sync', {
         method: 'POST',
         headers: {
-          accept: 'application/json',
+          'accept': 'application/json',
           'content-type': 'application/json',
-          'Api-Token': secret
+          'Api-Token': secret,
         },
-        body: reqBody
-      }
+        body: reqBody,
+      },
       )
 
-      if (sendActiveCampaignForm?.contact?.id?.length > 0 && body.bankLeadList) {
-        // If we have a created contact, add contact to the Bank Leads list (27)
+      if (sendActiveCampaignForm?.contact?.id?.length > 0) {
+        // If we have a created contact, add contact to a list
         const addContactToList: any = await $fetch(
           baseUrl + '/contactLists',
           {
             headers: {
-              accept: 'application/json',
+              'accept': 'application/json',
               'content-type': 'application/json',
-              'Api-Token': secret
+              'Api-Token': secret,
             },
             method: 'POST',
             body: {
               contactList: {
-                list: 27,
+                // add to Bank Lead list (27) if applicable otherwise to Unconfirmed (28)
+                list: body.bankLeadList ? 27 : 28,
                 contact: sendActiveCampaignForm?.contact?.id,
-                status: 1
-              }
-            }
-          }
+                status: 1,
+              },
+            },
+          },
         )
 
         if (addContactToList?.contactList?.id) {
           return { success: true }
         }
       } else {
-        if (sendActiveCampaignForm?.contact?.id?.length > 0) {
-          return {
-            success: true
-          }
+        return {
+          success: false,
         }
-        else {
-          return {
-            success: false
-          }
-        }
-        
       }
     } catch (e: any) {
       const _e: Error = e
@@ -92,7 +85,7 @@ export default defineEventHandler(
       setResponseStatus(400)
       return {
         success: false,
-        error: _e.message
+        error: _e.message,
       }
     }
   })
