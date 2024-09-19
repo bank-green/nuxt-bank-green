5afcfc63c0d267f1b693f7a73daf7a038127041c
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-09-19 19:03:56 +0200

    Move default parameters out of the function

--- composables/useSwiper.ts
+++ composables/useSwiper.ts
@@ -5,47 +5,49 @@ import type { SwiperOptions } from 'swiper/types'
 
 register()
 
+const SWIPER_DEFAULT_PARAMETERS: SwiperOptions = {
+  modules: [Pagination],
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 20,
+  navigation: false,
+  pagination: {
+    clickable: true,
+  },
+  breakpoints: {
+    1028: {
+      slidesPerView: 2,
+    },
+  },
+  injectStyles: [`
+    :host {
+      --swiper-theme-color: #87B448;
+      --swiper-pagination-bullet-size: 16px;
+      --swiper-pagination-bullet-width: 16px;
+      --swiper-pagination-bullet-height: 16px;
+      --swiper-pagination-bottom: 0px;
+      --swiper-pagination-bullet-inactive-color: #D9D9D9;
+      --swiper-pagination-bullet-inactive-opacity: 1;
+    }
+    .swiper {
+      padding-bottom: 34px
+    }
+    .swiper-pagination-bullet {
+      transition: transform 0.15s ease-out;
+    }
+    .swiper-pagination-bullet-active {
+      transform: scale(1.25);
+      transition: transform 0.15s ease-out;
+    }
+    `],
+}
+
 export function useSwiper(options?: SwiperOptions) {
   const swiperRef = ref<SwiperContainer>()
-  const swiperDefaultParams: SwiperOptions = {
-    modules: [Pagination],
-    slidesPerView: 1,
-    slidesPerGroup: 1,
-    spaceBetween: 20,
-    navigation: false,
-    pagination: {
-      clickable: true,
-    },
-    breakpoints: {
-      1028: {
-        slidesPerView: 2,
-      },
-    },
-    injectStyles: [`
-      :host {
-        --swiper-theme-color: #87B448;
-        --swiper-pagination-bullet-size: 16px;
-        --swiper-pagination-bullet-width: 16px;
-        --swiper-pagination-bullet-height: 16px;
-        --swiper-pagination-bottom: 0px;
-        --swiper-pagination-bullet-inactive-color: #D9D9D9;
-        --swiper-pagination-bullet-inactive-opacity: 1;
-      }
-      .swiper {
-        padding-bottom: 34px
-      }
-      .swiper-pagination-bullet {
-        transition: transform 0.15s ease-out;
-      }
-      .swiper-pagination-bullet-active {
-        transform: scale(1.25);
-        transition: transform 0.15s ease-out;
-      }
-      `],
-  }
+
   onMounted(() => {
     if (swiperRef.value) {
-      Object.assign(swiperRef.value, { ...swiperDefaultParams, ...options })
+      Object.assign(swiperRef.value, { ...SWIPER_DEFAULT_PARAMETERS, ...options })
       swiperRef.value.initialize()
     }
   })
