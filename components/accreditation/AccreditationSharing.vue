74e163898598328a34ca6b96d5baf8d4c67474c5
claudiabdm <44007726+claudiabdm@users.noreply.github.com>
2024-09-26 12:18:50 +0200

    Add steps with leaf

--- components/accreditation/AccreditationSharing.vue
+++ components/accreditation/AccreditationSharing.vue
@@ -6,49 +6,63 @@
         background-repeat: no-repeat;
         background-size: contain;
     "
-    class="flex flex-col items-center min-h-[1400px] px-6 lg:px-28 py-16 lg:py-20 bg-white"
+    class="flex flex-col gap-20 items-center min-h-[1400px] px-6 lg:px-28 py-16 lg:py-20 bg-white"
   >
-    <h1 class="text-2xl lg:text-4xl font-bold">
+    <h1 class="text-2xl lg:text-4xl font-bold text-gray-800">
       {{ title }}
     </h1>
+
+    <ol class="flex flex-col gap-16 max-w-5xl">
+      <AccreditationSharingStep
+        :num="1"
+        :step="step1"
+      />
+
+      <AccreditationSharingStep
+        :num="2"
+        :step="step2"
+      />
+    </ol>
+
+    <div />
   </section>
 </template>
 
 <script lang="ts" setup>
 import type { AccreditationpageDocumentData } from 'prismicio-types'
-
-interface Step {
-  title?: AccreditationpageDocumentData['step1_title'] | AccreditationpageDocumentData['step2_title']
-  description?: AccreditationpageDocumentData['step1_description'] | AccreditationpageDocumentData['step2_description']
-}
+import type { Step } from './AccreditationSharingStep.vue'
 
 interface Props {
   title?: AccreditationpageDocumentData['sharing_title']
-  steps?: {
+  steps: {
     step1: Step
     step2: Step
   }
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   title: 'With just a few clicks, you can do so much more...',
-  steps: () => ({
-    step1: {
-      title: 'Share the message',
-      description: [{
-        type: 'paragraph',
-        text: 'Spreading the word about green banking is crucial to help raise awareness about the importance of sustainable financial practices and let business owners know you appreciate their commitment to a greener future. Every share contributes to a larger movement for positive change.',
-        spans: [],
-      }],
-    },
-    step2: {
-      title: 'Check your own bank',
-      description: [{
-        type: 'paragraph',
-        text: 'Use our bank-checking tool to ensure your money is supporting environmentally responsible financial institutions. This tool helps you make informed decisions about where to bank, aligning your savings with your values and contributing to a more sustainable world.',
-        spans: [],
-      }],
-    },
-  }),
 })
+
+const step1 = {
+  title: props.steps.step1.title ?? 'Share the message',
+  description: props.steps.step1.description ?? [{
+    type: 'paragraph',
+    text: 'Spreading the word about green banking is crucial to help raise awareness about the importance of sustainable financial practices and let business owners know you appreciate their commitment to a greener future. Every share contributes to a larger movement for positive change.',
+    spans: [{
+      start: 211,
+      end: 277,
+      type: 'strong',
+    }],
+  }],
+}
+
+const step2 = {
+  title: props.steps.step2.title ?? 'Check your own bank',
+  description: props.steps.step2.description ?? [{
+    type: 'paragraph',
+    text: 'Use our bank-checking tool to ensure your money is supporting environmentally responsible financial institutions. This tool helps you make informed decisions about where to bank, aligning your savings with your values and contributing to a more sustainable world.',
+    spans: [],
+  }],
+}
 </script>
---
+++ components/accreditation/AccreditationSharingStep.vue
@@ -0,0 +1,56 @@
+<template>
+  <li
+    class="border border-gray-200 rounded-2xl lg:rounded-3xl bg-gray-light shadow-soft"
+  >
+    <div class="flex justify-center items-center w-10 lg:w-20 h-10 lg:h-20 text-white font-bold leaf">
+      <span class="text-2xl lg:text-5xl">{{ num }}</span>
+    </div>
+
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-20 px-6 lg:px-16 pt-6 pb-10 lg:py-10">
+      <div>
+        <h2 class="mb-4 text-2xl lg:text-3xl font-bold text-gray-800">
+          {{ step.title }}
+        </h2>
+        <PrismicRichText
+          :field="step.description"
+        />
+      </div>
+
+      <div>
+        <slot />
+      </div>
+    </div>
+  </li>
+</template>
+
+<script lang="ts" setup>
+import type { AccreditationpageDocumentData } from 'prismicio-types'
+
+export interface Step {
+  title?: AccreditationpageDocumentData['step1_title'] | AccreditationpageDocumentData['step2_title']
+  description?: AccreditationpageDocumentData['step1_description'] | AccreditationpageDocumentData['step2_description']
+}
+export interface Props {
+  step: Step
+  num: number
+}
+
+defineProps<Props>()
+</script>
+
+<style scoped>
+.leaf {
+  position: absolute;
+  transform: translateX(-25%) translateY(-25%);
+  background-image: url('/img/logos/bankgreen-logo.png');
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+@media screen and (min-width: 1028px) {
+  .leaf {
+    transform: translateX(-50%) translateY(25%);
+  }
+}
+</style>
