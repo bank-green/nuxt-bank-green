1aad94ba2adad2dc484930866754f0a94f279ca7
3DPros <altbillington@gmail.com>
2024-12-08 03:15:55 -0600

    Update sitemap.vue

--- pages/sitemap.vue
+++ pages/sitemap.vue
@@ -2,38 +2,28 @@
   <div class="page">
     <div class="bg-white">
       <div class="page-fade-in pt-28 ml-28 mt-8">
-        <div>
-          <ul>
-            <li
-              v-for="bank in banks"
-              :key="bank.tag"
-            >
-              <a
-                :href="`/banks/${bank.tag}`"
-                target="_blank"
-              >{{ bank.name }}</a>
-            </li>
-          </ul>
-        </div>
+        <ul>
+          <li
+            v-for="bank in banks"
+            :key="bank.tag"
+          >
+            <a
+              :href="`/banks/${bank.tag}`"
+              target="_blank"
+              rel="noopener noreferrer"
+            >{{ bank.name }}</a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
-import { getAllBanksList } from '../utils/banks.js'
-
-interface Bank {
-  name: string
-  tag: string
-  website: string
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const banks = await $axios.$get('https://example.com/api/banks')
+    return { banks }
+  },
 }
-
-const banks = ref<Bank[]>([])
-const loaded = ref<boolean>(false)
-
-onMounted(async () => {
-  banks.value = await getAllBanksList()
-})
 </script>
