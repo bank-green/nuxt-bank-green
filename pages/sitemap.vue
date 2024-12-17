db341fd50cbc6a63bd155892a02fb28bce31ebdd
3DPros <altbillington@gmail.com>
2024-12-17 02:04:14 -0600

    Update sitemap.vue

--- pages/sitemap.vue
+++ pages/sitemap.vue
@@ -3,9 +3,17 @@
     <div class="bg-white">
       <div class="page-fade-in pt-28 ml-28 mt-16">
         <ul>
-          <li v-for="bank in banks" :key="bank.tag">
+          <li
+            v-for="bank in banks"
+            :key="bank.tag"
+          >
             <p><a :href="`/banks/${bank.tag}`">{{ bank.name }}</a></p>
-            <p><a v-if="bank.sustainablenksPage" :href="`/sustainable-eco-banks/${bank.tag}`">{{ bank.name }}</a></p>
+            <p>
+              <a
+                v-if="bank.sustainablenksPage"
+                :href="`/sustainable-eco-banks/${bank.tag}`"
+              >{{ bank.name }}</a>
+            </p>
           </li>
         </ul>
       </div>
