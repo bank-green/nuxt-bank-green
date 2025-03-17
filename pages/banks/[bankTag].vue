9dbcdbc4491702527657f1991f6bd0a7a5611078
mcromp <crompton.ml@gmail.com>
2025-03-17 09:51:31 +0100

    GROOT-375 Refactor to fix build

--- pages/banks/[bankTag].vue
+++ pages/banks/[bankTag].vue
@@ -65,4 +65,16 @@ const getFieldOrDefault = (fieldName: string) => {
     return defaultFields.value[fieldName]
   }
 }
+
+const getRating = ({ commentary }: NonNullable<BrandByTagQueryQuery['brand']>): string => {
+  const inheritedRating = commentary?.ratingInherited?.toLocaleLowerCase()
+  const isCreditUnion = commentary?.institutionType?.[0]?.name === 'Credit Union'
+  const isRatingUnknown = commentary?.rating === 'unknown'
+
+  // for credit unions we want to overwrite a brand's rating to 'good' if 'unknown'
+  if (isCreditUnion && isRatingUnknown) {
+    return 'good'
+  }
+  return inheritedRating || ''
+}
 </script>
