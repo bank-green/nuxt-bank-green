a7b5f68b753d37b1c7823903444722f3612596f5
moshaid <alabidunmoshood@gmail.com>
2025-04-01 01:19:48 +0100

    console error fixed

--- components/CheckList.vue
+++ components/CheckList.vue
@@ -1,7 +1,6 @@
 <template>
   <div
-    :class="`${
-      dark ? 'text-white' : 'bg-white text-gray-700 md:shadow-md'
+    :class="`${dark ? 'text-white' : 'bg-white text-gray-700 md:shadow-md'
     } rounded-2xl px-8 py-10 space-y-6`"
   >
     <div
@@ -10,8 +9,7 @@
       class="flex items-start"
     >
       <img
-        :class="`${
-          dark ? 'w-7 h-7' : 'bg-pistachio-green w-6 h-6'
+        :class="`${dark ? 'w-7 h-7' : 'bg-pistachio-green w-6 h-6'
         } p-1 mt-0.5 mr-4 rounded-full`"
         src="/img/icons/check.svg"
         alt=""
@@ -29,10 +27,13 @@
 </template>
 
 <script setup lang="ts">
-withDefaults(defineProps<{
+import { defineProps } from 'vue'
+import type { RichTextField } from '@prismicio/types'
+
+const props = defineProps<{
   list: (string | RichTextField)[]
-  dark: boolean
-}>(), {
-  dark: false,
-})
+  dark?: boolean
+}>()
+
+const dark = props.dark ?? false
 </script>
