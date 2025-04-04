75d0eba583fdaba77b2c2c7ed8a216b7666da05a
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-04-04 14:26:51 +0100

    Add GTM script in <head>, GTM <noscript> in <body>

--- App.vue
+++ App.vue
@@ -4,7 +4,16 @@
     class="min-h-screen flex flex-col"
     @mouseleave="onExitIntent"
   >
-    <NuxtLoadingIndicator :color="'#7BB123'" :height="6" />
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMGKCN3"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <NuxtLoadingIndicator
+      :color="'#7BB123'"
+      :height="6"
+    />
     <ClientOnly>
       <CookieBanner />
     </ClientOnly>
@@ -21,22 +30,40 @@
 </template>
 
 <script setup>
-
 const openSwitchSurveyModal = ref(false)
 const hasUserSeenExitIntentModal = useCookie('bg.seenExitIntent', {
-  default: () => false
+  default: () => false,
 })
 
 const route = useRoute()
 
-function onExitIntent () {
+function onExitIntent() {
   if (hasUserSeenExitIntentModal.value) { return }
   if (openSwitchSurveyModal.value) { return }
   if (route.path.includes('/impact')) { return }
   openSwitchSurveyModal.value = true
   hasUserSeenExitIntentModal.value = true
 }
+
+// <!-- Google Tag Manager -->
+useHead({
+  script: [
+    {
+      hid: 'gtm-script',
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-KMGKCN3');`,
+      type: 'text/javascript',
+    },
+  ],
+  __dangerouslyDisableSanitizersByTagID: {
+    'gtm-script': ['innerHTML'],
+  },
+})
 </script>
+
 <style>
 body {
   background: #f9fafb;
