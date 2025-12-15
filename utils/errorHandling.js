a1eb0e72840aa5305a5d426d36a236368b83f9d1
nerisina <neri.esparza@gmail.com>
2025-12-15 08:15:57 -0600

    refactor: improve error logging and clean up console statements in components

--- utils/errorHandling.js
+++ utils/errorHandling.js
@@ -2,66 +2,66 @@ const httpErrorHandling = {
   /**
    * Show toast with error message
    */
-  handleBadRequest (error) {
-    console.log(error.response)
-    const title = error.response?.data?.message || error.message
-    notifications.addNotification({ icon: 'x-circle', color: 'red', title })
+  handleBadRequest(error) {
+    console.error(error.response);
+    const title = error.response?.data?.message || error.message;
+    notifications.addNotification({ icon: 'x-circle', color: 'red', title });
   },
 
   /**
    * Log out user on 401 requests
    */
-  handleBadAuth (error) {
-    console.log(error.response)
-    const title = 'Authentication failed'
-    const description = error.response?.data || error.message
+  handleBadAuth(error) {
+    console.error(error.response);
+    const title = 'Authentication failed';
+    const description = error.response?.data || error.message;
     notifications.addNotification({
       icon: 'x-circle',
       color: 'red',
       title,
-      description
-    })
+      description,
+    });
   },
 
   /**
    *
    * @param { import('axios').AxiosInstance } instance
    */
-  setInterceptors (instance) {
+  setInterceptors(instance) {
     instance.interceptors.request.use(
-      (config) => {
+      config => {
         // add timestamp so we never cache requests
         config.url +=
           (config.url.indexOf('?') > 0 ? '&' : '?') +
-          `t=${new Date().getTime()}`
-        return config
+          `t=${new Date().getTime()}`;
+        return config;
       },
-      (error) => {
-        return Promise.reject(error)
+      error => {
+        return Promise.reject(error);
       }
-    )
+    );
 
     // Add a response interceptor
     instance.interceptors.response.use(
-      (response) => {
-        return response
+      response => {
+        return response;
       },
-      (error) => {
+      error => {
         // handle all HTTP errors here
         // if 400 => show a toast
         if (error.response?.status === 400) {
-          this.handleBadRequest(error)
+          this.handleBadRequest(error);
         } else if (error.response?.status === 401) {
-          this.handleBadAuth(error)
+          this.handleBadAuth(error);
         } else if (error.isAxiosError && error.message) {
-          this.handleBadRequest(error)
+          this.handleBadRequest(error);
         } else {
-          console.error('some other response threw an error', { error })
+          console.error('some other response threw an error', { error });
         }
-        return Promise.reject(error)
+        return Promise.reject(error);
       }
-    )
-  }
-}
+    );
+  },
+};
 
-export default httpErrorHandling
+export default httpErrorHandling;
