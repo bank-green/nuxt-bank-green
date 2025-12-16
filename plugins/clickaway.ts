a8931792d9d11b67b545a5bc753ab4ea563307be
nerisina <neri.esparza@gmail.com>
2025-12-15 18:12:29 -0600

    refactor: improve clickaway directive with TypeScript typings and event handling

--- plugins/clickaway.ts
+++ plugins/clickaway.ts
@@ -1,18 +1,29 @@
-import { defineNuxtPlugin } from '#app'
-import { DirectiveBinding, VNode } from 'vue'
+import type { DirectiveBinding, VNode } from 'vue';
+import { defineNuxtPlugin } from '#app';
 
-export default defineNuxtPlugin((nuxtApp) => {
+interface HTMLElementWithClickOutside extends HTMLElement {
+  clickOutsideEvent?: (event: Event) => void;
+}
+
+export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.vueApp.directive('clickaway', {
-    mounted (el: any, binding: DirectiveBinding<any>, _: VNode<any, any>) {
-      el.clickOutsideEvent = (event: { target: any }) => {
-        if (!(el === event.target || el.contains(event.target))) {
-          binding.value(el, event)
+    mounted(
+      el: HTMLElementWithClickOutside,
+      binding: DirectiveBinding,
+      _: VNode
+    ) {
+      el.clickOutsideEvent = (event: Event) => {
+        const target = event.target as Node;
+        if (!(el === target || el.contains(target))) {
+          binding.value(el, event);
         }
-      }
-      document.addEventListener('click', el.clickOutsideEvent)
+      };
+      document.addEventListener('click', el.clickOutsideEvent);
     },
-    unmounted (el) {
-      document.removeEventListener('click', el.clickOutsideEvent)
-    }
-  })
-})
+    unmounted(el: HTMLElementWithClickOutside) {
+      if (el.clickOutsideEvent) {
+        document.removeEventListener('click', el.clickOutsideEvent);
+      }
+    },
+  });
+});
